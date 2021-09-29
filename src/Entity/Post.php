d78dd2808c0296e4440d80a331a1856b35e30005
unknown <franklintche@yahoo.fr>
2021-09-29 11:29:43 +0100

    Add data

---
+++ src/Entity/Post.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read:collection"}},
+ *     denormalizationContext={"groups"={"write:item","write:Post"}},
+ *     collectionOperations={
+ *     "get",
+ *     "post"},
+ *     itemOperations={
+ *     "put","delete","get"={"normalization_context"={"groups"={"read:item"}}}
+ *     }
+ *     )
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"read:collection"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"read:collection","read:item", "write:item"})
+     * @Assert\Length(min=5,groups={"create:collection"})
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"read:collection","read:item", "write:item"})
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Groups({"read:item", "write:item"})
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"read:item"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts", cascade={"persist"})
+     * @Groups({"read:item", "write:Post"})
+     * @Assert\Valid()
+     */
+    private $category;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
